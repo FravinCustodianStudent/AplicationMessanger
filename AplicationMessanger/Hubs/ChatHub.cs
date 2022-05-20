53b4ff057976de4f090d766cc7626d3f9c242101
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-05-20 16:16:34 +0300

    Chat v0.3

--- AplicationMessanger/Hubs/ChatHub.cs
+++ AplicationMessanger/Hubs/ChatHub.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using AplicationMessanger.Areas.Identity.Data;
+using AplicationMessanger.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
 
 namespace AplicationMessanger.Hubs
 {
     public class ChatHub : Hub
     {
-        public Task SendMessageToChat(string chatId,string message)
+        private readonly AplicationMessangerContext _bd;
+        private readonly UserManager<AplicationMessangerUser> _userManager;
+        public ChatHub(AplicationMessangerContext bd, UserManager<AplicationMessangerUser> userManager)
         {
-            return Clients.Group(chatId).SendAsync("ReceiveMessage",message);
+            _bd = bd;
+            _userManager = userManager;
+        }
+        public Task SendMessageToChat(string chatId,string message,string date,string userId)
+        {
+            DateTime now = DateTime.Parse(date);
+            var User = _userManager.Users.FirstOrDefault(u => u.Id == userId);
+            string pathToImage = User.Avatar;
+            string userName = User.UserName;
+            string time = now.ToString("MM/dd/yyyy HH:mm");
+            return Clients.Group(chatId).SendAsync("ReceiveMessage",message,time,userId,pathToImage,userName);
         }
 
         public Task JoinGroup(string chatId)
